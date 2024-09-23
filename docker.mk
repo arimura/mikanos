a47d5aa4ca6381c70b90ba7611a9ff5de4758fb8
arimura <33759663+arimura@users.noreply.github.com>
2024-09-23 22:54:36 +0900

    Add target

--- docker.mk
+++ docker.mk
@@ -2,4 +2,21 @@
 HOME=/home/vscode
 
 run-qemu:
-	${HOME}/osbook/devenv/run_qemu.sh ${EFI}
+	
+
+EFI=
+disk.img:
+ifeq ($(EFI),)
+	$(error "EFI is not set")
+endif	
+	qemu-img create -f raw disk.img 200M
+	mkfs.fat -n 'MIKAN OS' -s 2 -f 2 -R 32 -F 32 disk.img
+	mkdir -p mnt
+	sudo mount -o loop disk.img mnt
+	sudo mkdir -p mnt/EFI/BOOT
+	sudo cp $(EFI) mnt/EFI/BOOT/BOOTX64.EFI
+	sudo umount mnt
+
+clean:
+	rm -f disk.img
+
