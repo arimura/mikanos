4491bcacf064e1fbfe1121dd633d569a385f6d4d
arimura <33759663+arimura@users.noreply.github.com>
2024-09-25 09:36:44 +0900

    Add target

--- docker.mk
+++ docker.mk
@@ -2,7 +2,10 @@
 HOME=/home/vscode
 
 run-qemu:
-	
+	qemu-system-x86_64 \
+	  -drive if=pflash,file=$(HOME)/osbook/devenv/OVMF_CODE.fd \
+	  -drive if=pflash,file=$(HOME)/osbook/devenv/OVMF_VARS.fd \
+	  -hda disk.img
 
 EFI=
 disk.img:
