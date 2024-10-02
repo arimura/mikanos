6980c93d2f3110508c66fcd65472688394c0ac23
arimura <33759663+arimura@users.noreply.github.com>
2024-10-02 09:27:59 +0900

    Add target

--- docker.mk
+++ docker.mk
@@ -1,6 +1,8 @@
 # makefile executed within docker container
 HOME=/home/vscode
 
+all: disk.img run-qemu
+
 run-qemu:
 	qemu-system-x86_64 \
 	  -drive if=pflash,file=$(HOME)/osbook/devenv/OVMF_CODE.fd \
@@ -19,6 +21,10 @@ endif
 	sudo mkdir -p mnt/EFI/BOOT
 	sudo cp $(EFI) mnt/EFI/BOOT/BOOTX64.EFI
 	sudo umount mnt
+
+update-tools_def:
+	sed -i -e '/DEFINE CLANG38_X64_TARGET/s/-target x86_64-pc-linux-gnu/-target x86_64-linux-gnu/' \
+       -e '/DEBUG_CLANG38_X64_CC_FLAGS/s/\(.*\)/\1 -I\/usr\/x86_64-linux-gnu\/include/' /home/vscode/edk2/Conf/tools_def.txt
 
 clean:
 	rm -f disk.img
