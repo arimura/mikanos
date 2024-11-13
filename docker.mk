b0cf5974df470437e454dfc924caea4abcdfce8f
arimura <33759663+arimura@users.noreply.github.com>
2024-11-13 09:06:09 +0900

    Add kernel build target

--- docker.mk
+++ docker.mk
@@ -32,6 +32,12 @@ update-tools_def:
 	sed -i -e '/DEFINE CLANG38_X64_TARGET/s/-target x86_64-pc-linux-gnu/-target x86_64-linux-gnu/' \
        -e '/DEBUG_CLANG38_X64_CC_FLAGS/s/\(.*\)/\1 -I\/usr\/x86_64-linux-gnu\/include/' /home/vscode/edk2/Conf/tools_def.txt
 
+kernel.elf:
+	clang++ -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone \
+	  -fno-exceptions -fno-rtti -std=c++17 -c my_mikanos/kernel/main.cpp
+	ld.lld --entry KernelMain -z norelro --image-base 0x100000 --static \
+	  -o kernel.elf main.o
+
 clean:
 	rm -f disk.img
 
