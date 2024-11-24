99594142e627c8d0193bb01d77a408b0ed8a2766
arimura <33759663+arimura@users.noreply.github.com>
2024-11-24 21:20:01 +0900

    Write rectangle

--- my_mikanos/kernel/frame_buffer_config.hpp
+++ my_mikanos/kernel/frame_buffer_config.hpp
@@ -4,11 +4,11 @@
 
 enum PixelFormat {
     kPixelRGBResv8BitPerColor,
-    kPixelNGRResv8BitPerColor,
+    kPixelBGRResv8BitPerColor,
 };
 
 struct FrameBufferConfig {
-    uint8_t* frameBuffer;
+    uint8_t* frame_buffer;
     uint32_t pixels_per_scan_line;   
     uint32_t horizontal_resolution;
     uint32_t vertical_resolution;
