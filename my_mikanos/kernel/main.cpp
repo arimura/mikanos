c3f6171b559ecb0caf22e42450b170f18f0c7e5e
arimura <33759663+arimura@users.noreply.github.com>
2024-11-28 08:58:50 +0900

    Format

--- my_mikanos/kernel/main.cpp
+++ my_mikanos/kernel/main.cpp
@@ -4,59 +4,76 @@
 #include "frame_buffer_config.hpp"
 
 // #@range_begin(write_pixel)
-struct PixelColor {
-    uint8_t r, g ,b;
+struct PixelColor
+{
+    uint8_t r, g, b;
 };
 
 // #@@range_begin(pixel_writer)
-class PixelWriter {
-    public:
-        PixelWriter(const FrameBufferConfig& config) : config_{config}{
-        }        
-        virtual ~PixelWriter() = default;
-        virtual void Write(int x, int y, const PixelColor& c) = 0;
+class PixelWriter
+{
+public:
+    PixelWriter(const FrameBufferConfig &config) : config_{config}
+    {
+    }
+    virtual ~PixelWriter() = default;
+    virtual void Write(int x, int y, const PixelColor &c) = 0;
 
-        protected:
-            uint8_t* PixelAt(int x, int y) {
-                return config_.frame_buffer + 4 * (config_.pixels_per_scan_line *y + x);
-            }
-        private:
-            const FrameBufferConfig& config_;
+protected:
+    uint8_t *PixelAt(int x, int y)
+    {
+        return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
+    }
+
+private:
+    const FrameBufferConfig &config_;
 };
 // #@@range_end(pixel_writer)
 
-int WritePixel(const FrameBufferConfig& config,
-               int x, int y, const PixelColor& c){
+int WritePixel(const FrameBufferConfig &config,
+               int x, int y, const PixelColor &c)
+{
     const int pixel_position = config.pixels_per_scan_line * y + x;
-    if(config.pixel_format == kPixelRGBResv8BitPerColor) {
-        uint8_t* p = &config.frame_buffer[4 * pixel_position];
+    if (config.pixel_format == kPixelRGBResv8BitPerColor)
+    {
+        uint8_t *p = &config.frame_buffer[4 * pixel_position];
         p[0] = c.r;
         p[1] = c.g;
         p[2] = c.b;
-    } else if (config.pixel_format == kPixelBGRResv8BitPerColor) {
-        uint8_t* p = &config.frame_buffer[4 * pixel_position];
+    }
+    else if (config.pixel_format == kPixelBGRResv8BitPerColor)
+    {
+        uint8_t *p = &config.frame_buffer[4 * pixel_position];
         p[0] = c.b;
         p[1] = c.g;
         p[2] = c.r;
-    } else {
-        return -1; 
+    }
+    else
+    {
+        return -1;
     }
     return 0;
 }
 // #@range_end(write_pixel)
 
 // #@@range_begin(call_write_pixel)
-extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
-    for(int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
-        for(int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
+extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
+{
+    for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x)
+    {
+        for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y)
+        {
             WritePixel(frame_buffer_config, x, y, {255, 255, 255});
         }
     }
-    for (int x = 0; x < 200; ++x){
-        for(int y = 0; y < 100; ++y) {
-            WritePixel(frame_buffer_config, 100 + x, 100 + y, {0,255,0});
+    for (int x = 0; x < 200; ++x)
+    {
+        for (int y = 0; y < 100; ++y)
+        {
+            WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
         }
     }
 
-    while(1) __asm__("hlt");
+    while (1)
+        __asm__("hlt");
 }
