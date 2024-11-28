13f3505ada87e2297f319dca0d54b907462f0d29
arimura <33759663+arimura@users.noreply.github.com>
2024-11-28 09:33:53 +0900

    Use pixel writer

--- my_mikanos/kernel/main.cpp
+++ my_mikanos/kernel/main.cpp
@@ -97,21 +97,35 @@ int WritePixel(const FrameBufferConfig &config,
 }
 // #@range_end(write_pixel)
 
+char pixel_writer_buf[sizeof(RGBResv8bitPerColorPixelWriter)];
+PixelWriter* pixel_writer;
+
 // #@@range_begin(call_write_pixel)
 extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
 {
+    switch (frame_buffer_config.pixel_format){
+        case kPixelRGBResv8BitPerColor:
+            pixel_writer = new(pixel_writer_buf)
+                RGBResv8bitPerColorPixelWriter{frame_buffer_config};
+            break;
+        case kPixelBGRResv8BitPerColor:
+            pixel_writer = new(pixel_writer_buf)
+                BGRResv8bitPerColorPixelWriter{frame_buffer_config};
+            break;
+    }
+
     for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x)
     {
         for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y)
         {
-            WritePixel(frame_buffer_config, x, y, {255, 255, 255});
+            pixel_writer->Write(x, y, {255, 255, 255});
         }
     }
     for (int x = 0; x < 200; ++x)
     {
         for (int y = 0; y < 100; ++y)
         {
-            WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
+            pixel_writer->Write(x, y, {0, 255, 0});
         }
     }
 
