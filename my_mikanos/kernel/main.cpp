c277ab39d83b141a255ae3053ddfaaf0c2280c9d
arimura <33759663+arimura@users.noreply.github.com>
2024-11-28 12:38:12 +0900

    Remove unused

--- my_mikanos/kernel/main.cpp
+++ my_mikanos/kernel/main.cpp
@@ -71,32 +71,6 @@ void operator delete(void *obj) noexcept
 }
 // #@@range_end(placement_new)
 
-int WritePixel(const FrameBufferConfig &config,
-               int x, int y, const PixelColor &c)
-{
-    const int pixel_position = config.pixels_per_scan_line * y + x;
-    if (config.pixel_format == kPixelRGBResv8BitPerColor)
-    {
-        uint8_t *p = &config.frame_buffer[4 * pixel_position];
-        p[0] = c.r;
-        p[1] = c.g;
-        p[2] = c.b;
-    }
-    else if (config.pixel_format == kPixelBGRResv8BitPerColor)
-    {
-        uint8_t *p = &config.frame_buffer[4 * pixel_position];
-        p[0] = c.b;
-        p[1] = c.g;
-        p[2] = c.r;
-    }
-    else
-    {
-        return -1;
-    }
-    return 0;
-}
-// #@range_end(write_pixel)
-
 char pixel_writer_buf[sizeof(RGBResv8bitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
