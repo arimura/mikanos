8fc61c036f1a67134029a3744812bf23b543127e
arimura <33759663+arimura@users.noreply.github.com>
2025-01-14 09:28:51 +0900

    Remove unsed

--- docker.mk
+++ docker.mk
@@ -19,18 +19,6 @@ ifeq ($(EFI),)
 endif
 	/home/vscode/osbook/devenv/run_qemu.sh $(EFI) $(KERNEL_DIR)/$(KERNEL)
 
-disk.img:
-ifeq ($(EFI),)
-	$(error "EFI is not set")
-endif
-	qemu-img create -f raw disk.img 200M
-	mkfs.fat -n 'MIKAN OS' -s 2 -f 2 -R 32 -F 32 disk.img
-	mkdir -p mnt
-	sudo mount -o loop disk.img mnt
-	sudo mkdir -p mnt/EFI/BOOT
-	sudo cp $(EFI) mnt/EFI/BOOT/BOOTX64.EFI
-	sudo umount mnt
-
 update-tools_def:
 	sed -i -e '/DEFINE CLANG38_X64_TARGET/s/-target x86_64-pc-linux-gnu/-target x86_64-linux-gnu/' \
        -e '/DEBUG_CLANG38_X64_CC_FLAGS/s/\([^[:cntrl:]]*\)/\1 -I\/usr\/x86_64-linux-gnu\/include/' /home/vscode/edk2/Conf/tools_def.txt
