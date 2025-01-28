45b5c9ba5effb46602dc08c0db34eab2a91fd0af
arimura <33759663+arimura@users.noreply.github.com>
2025-01-28 22:03:52 +0900

    fix

--- my_mikanos/kernel/main.cpp
+++ my_mikanos/kernel/main.cpp
@@ -19,22 +19,6 @@ void operator delete(void *obj) noexcept
 char pixel_writer_buf[sizeof(RGBResv8bitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
-char console_buf[sizeof(Console)];
-Console* console;
-
-int printk(const char* format, ...) {
-    va_list ap;
-    int result;
-    char s[1024];
-
-    va_start(ap, format);
-    result = vsprintf(s, format, ap);
-    va_end(ap);
-
-    console->PutString(s);
-    return result;
-}
-
 extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
 {
     switch (frame_buffer_config.pixel_format){
@@ -56,10 +40,11 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
         }
     }
 
-    console =  new(console_buf)Console{*pixel_writer, {0,0,0}, {255,255,255}};
+    Console consle{*pixel_writer, {0,0,0}, {255,255,255}};
+    char buf[128];
 
-    for (int i =0; i < 27; ++i){
-        printk("printK: %d\n", i);
+    for (int i = 0; i < 27; ++i){
+        sprintf(buf, "line %d\n", i);
     }
     while (1)
         __asm__("hlt");
