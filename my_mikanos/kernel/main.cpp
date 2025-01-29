cde73354a42aa96f9fb3a5c05f172ac3da76b8df
arimura <33759663+arimura@users.noreply.github.com>
2025-01-29 09:10:30 +0900

    Fix

--- my_mikanos/kernel/main.cpp
+++ my_mikanos/kernel/main.cpp
@@ -40,11 +40,12 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
         }
     }
 
-    Console consle{*pixel_writer, {0,0,0}, {255,255,255}};
+    Console console{*pixel_writer, {0,0,0}, {255,255,255}};
     char buf[128];
 
     for (int i = 0; i < 27; ++i){
         sprintf(buf, "line %d\n", i);
+        console.PutString(buf);
     }
     while (1)
         __asm__("hlt");
