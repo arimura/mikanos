203b2bec9057ee0649856fe7dae64cb6a2da4066
arimura <33759663+arimura@users.noreply.github.com>
2025-02-15 10:36:40 +0900

    Impl

--- my_mikanos/kernel/mouse.cpp
+++ my_mikanos/kernel/mouse.cpp
@@ -37,7 +37,7 @@ namespace {
       for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
         if (mouse_cursor_shape[dy][dx] == '@') {
           pixel_writer->Write(position.x + dx, position.y + dy,
-              { 255, 255, 255 });
+              { 0, 0, 0 });
         } else if (mouse_cursor_shape[dy][dx] == '.') {
           pixel_writer->Write(position.x + dx, position.y + dy,
               { 255, 255, 255 });
@@ -45,4 +45,27 @@ namespace {
       }
     }
   }
+
+  void EraseMouseCursor(PixelWriter* pixel_writer, Vector2D<int> position, PixelColor erase_color) {
+    for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
+      for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
+        if (mouse_cursor_shape[dy][dx] != ' ') {
+          pixel_writer->Write(position.x + dx, position.y + dy, erase_color);
+        }
+      }
+    }
+  }
+}
+
+MouseCursor::MouseCursor(PixelWriter* writer, PixelColor erase_color, Vector2D<int> initial_position)
+    : pixel_writer_ { writer }
+    , erase_color_ { erase_color }
+    , position_ { initial_position } {
+  DrawMouseCursor(pixel_writer_, position_);
+}
+
+void MouseCursor::MoveRelative(Vector2D<int> displacement) {
+  EraseMouseCursor(pixel_writer_, position_, erase_color_);
+  position_ += displacement;
+  DrawMouseCursor(pixel_writer_, position_);
 }
