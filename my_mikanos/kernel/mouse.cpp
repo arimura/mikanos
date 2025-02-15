9a37bb5cb1301144b5645f7ebb8a8b3b82ac7e6c
arimura <33759663+arimura@users.noreply.github.com>
2025-02-15 09:58:14 +0900

    Impl

--- my_mikanos/kernel/mouse.cpp
+++ my_mikanos/kernel/mouse.cpp
@@ -1,34 +1,52 @@
 #include "mouse.hpp"
 #include "graphics.hpp"
 
-namespace {
+namespace
+{
     const int kMouseCursorWidth = 15;
     const int kMouseCursorHeight = 24;
     const char mouse_cursor_shape[kMouseCursorHeight]
-    [kMouseCursorWidth + 1] = {
-        "@              ",
-        "@@             ",
-        "@.@            ",
-        "@..@           ",
-        "@...@          ",
-        "@....@         ",
-        "@.....@        ",
-        "@......@       ",
-        "@.......@      ",
-        "@........@     ",
-        "@.........@    ",
-        "@..........@   ",
-        "@...........@  ",
-        "@............@ ",
-        "@......@@@@@@@@",
-        "@......@       ",
-        "@....@@.@      ",
-        "@...@ @.@      ",
-        "@..@   @.@     ",
-        "@.@    @.@     ",
-        "@@      @.@    ",
-        "@       @.@    ",
-        "         @.@   ",
-        "         @@@   ",
-      };
+                                 [kMouseCursorWidth + 1] = {
+                                     "@              ",
+                                     "@@             ",
+                                     "@.@            ",
+                                     "@..@           ",
+                                     "@...@          ",
+                                     "@....@         ",
+                                     "@.....@        ",
+                                     "@......@       ",
+                                     "@.......@      ",
+                                     "@........@     ",
+                                     "@.........@    ",
+                                     "@..........@   ",
+                                     "@...........@  ",
+                                     "@............@ ",
+                                     "@......@@@@@@@@",
+                                     "@......@       ",
+                                     "@....@@.@      ",
+                                     "@...@ @.@      ",
+                                     "@..@   @.@     ",
+                                     "@.@    @.@     ",
+                                     "@@      @.@    ",
+                                     "@       @.@    ",
+                                     "         @.@   ",
+                                     "         @@@   ",
+    };
+
+    void DrawMouseCursor(PixelWriter *pixel_writer, Vector2D<int> position)
+    {
+        for (int dy = 0; dy < kMouseCursorHeight; ++dy)
+        {
+            for (int dx = 0; dx < kMouseCursorWidth; ++dx)
+            {
+                if (mouse_cursor_shape[dy][dx] == '@')
+                {
+                    pixel_writer->Write(position.x + dx, position.y + dy, {255, 255, 255});
+                }
+                else if (mouse_cursor_shape[dy][dx] == '.') {
+                    pixel_writer->Write(position.x + dx, position.y + dy, {255,255,255});
+                }
+            }
+        }
+    }
 }
