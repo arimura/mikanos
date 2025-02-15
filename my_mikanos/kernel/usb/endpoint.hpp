ebf0e969bccb388a4ff41f06c8b86ef7e24ed910
arimura <33759663+arimura@users.noreply.github.com>
2025-02-15 10:47:58 +0900

    Impl

---
+++ my_mikanos/kernel/usb/endpoint.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "error.hpp"
+
+namespace usb {
+  enum class EndpointType {
+    kControl = 0,
+    kIsochronous = 1,
+    kBulk = 2,
+    kInterrupt = 3,
+  };
+
+  class EndpointID {
+public:
+    constexpr EndpointID()
+        : addr_ { 0 } { }
+
+private:
+    int addr_;
+  };
+}
