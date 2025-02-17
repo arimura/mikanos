19b4eda3e607eb83c39c7dc9adf32391c8c6898d
arimura <33759663+arimura@users.noreply.github.com>
2025-02-17 21:44:06 +0900

    Impl

--- my_mikanos/kernel/main.cpp
+++ my_mikanos/kernel/main.cpp
@@ -1,19 +1,28 @@
-#include <cstdint>
 #include <cstddef>
+#include <cstdint>
 #include <cstdio>
 
+#include <numeric>
+#include <vector>
+
+#include "console.hpp"
+#include "font.hpp"
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
-#include "font.hpp"
-#include "console.hpp"
+#include "logger.hpp"
+#include "mouse.hpp"
 #include "pci.hpp"
+#include "usb/classdriver/mouse.hpp"
+#include "usb/device.hpp"
+#include "usb/memory.hpp"
+#include "usb/xhci/trb.hpp"
+#include "usb/xhci/xhci.hpp"
 
-void operator delete(void *obj) noexcept
-{
+void operator delete(void* obj) noexcept {
 }
 
-const PixelColor kDesktopBGColor{45, 118, 237};
-const PixelColor kDesktopFGColor{255,255,255};
+const PixelColor kDesktopBGColor { 45, 118, 237 };
+const PixelColor kDesktopFGColor { 255, 255, 255 };
 
 const int kMouseCursorWidth = 15;
 const int kMouseCursorHeight = 24;
@@ -51,93 +60,158 @@ char console_buf[sizeof(Console)];
 Console* console;
 
 int printk(const char* format, ...) {
-    va_list ap;
-    int result;
-    char s[1024];
+  va_list ap;
+  int result;
+  char s[1024];
 
-    va_start(ap, format);
-    result = vsprintf(s, format, ap);
-    va_end(ap);
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
 
-    console->PutString(s);
-    return result;
+  console->PutString(s);
+  return result;
 }
 
-extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
-{
-    switch (frame_buffer_config.pixel_format){
-        case kPixelRGBResv8BitPerColor:
-            pixel_writer = new(pixel_writer_buf)
-                RGBResv8bitPerColorPixelWriter{frame_buffer_config};
-            break;
-        case kPixelBGRResv8BitPerColor:
-            pixel_writer = new(pixel_writer_buf)
-                BGRResv8bitPerColorPixelWriter{frame_buffer_config};
-            break;
+char mouse_cursor_buf[sizeof(MouseCursor)];
+MouseCursor* mouser_cursor;
+
+void MouseObserver(int8_t displacement_x, int8_t displacement_y) {
+  mouser_cursor->MoveRelative({ displacement_x, displacement_y });
+}
+
+void SwitchThci2Xhci(const pci::Device& xhc_dev) {
+  bool intel_ehc_exist = false;
+  for (int i = 0; i < pci::num_device; ++i) {
+    if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x20u) && 0x8086 == pci::ReadVendorId(pci::devices[i])) {
+      intel_ehc_exist = true;
+      break;
     }
+  }
+  if (!intel_ehc_exist) {
+    return;
+  }
 
-    const int kFrameWidth = frame_buffer_config.horizontal_resolution;
-    const int kFrameHeight = frame_buffer_config.vertical_resolution; 
+  uint32_t superspeed_ports = pci::ReadConfReg(xhc_dev, 0xdc);
+  pci::WriteConfReg(xhc_dev, 0xd8, superspeed_ports);
+  uint32_t ehci2xhci_ports = pci::ReadConfReg(xhc_dev, 0xd4);
+  pci::WriteConfReg(xhc_dev, 0xd0, ehci2xhci_ports);
+  Log(kDebug, "SwitchEhci2Xhci: SS = %02, xHCI = %02x\n",
+      superspeed_ports, ehci2xhci_ports);
+}
 
-    FillRectangle(*pixel_writer,
-                    {0,0},
-                    {kFrameWidth, kFrameHeight - 50},
-                    kDesktopBGColor);
-    FillRectangle(*pixel_writer,
-                    {0, kFrameHeight - 15},
-                    {kFrameWidth, 50},
-                    {1,8,17});
-    FillRectangle(*pixel_writer,
-                    {0, kFrameHeight - 15},
-                    {kFrameWidth / 5, 50},
-                    {80,80,80});
-    DrawRectangle(*pixel_writer,
-                    {10, kFrameHeight - 40},
-                    {30,30},
-                    {160, 160, 160});
+extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+  switch (frame_buffer_config.pixel_format) {
+  case kPixelRGBResv8BitPerColor:
+    pixel_writer = new (pixel_writer_buf)
+        RGBResv8bitPerColorPixelWriter { frame_buffer_config };
+    break;
+  case kPixelBGRResv8BitPerColor:
+    pixel_writer = new (pixel_writer_buf)
+        BGRResv8bitPerColorPixelWriter { frame_buffer_config };
+    break;
+  }
 
-    console = new(console_buf) Console{*pixel_writer,
-        kDesktopFGColor, kDesktopBGColor
-    };
-    printk("Welcome to MikanOS!\n");
+  const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+  const int kFrameHeight = frame_buffer_config.vertical_resolution;
 
-    for(int dy = 0; dy < kMouseCursorHeight; ++dy) {
-        for(int dx = 0; dx < kMouseCursorWidth; ++dx) {
-            if(mouse_cursor_shape[dy][dx] == '@'){
-                pixel_writer->Write(200 + dx, 100 + dy, {0,0,0});
-            }else if(mouse_cursor_shape[dy][dx] == '.'){
-                pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
-            }
-        }
+  FillRectangle(*pixel_writer,
+      { 0, 0 },
+      { kFrameWidth, kFrameHeight - 50 },
+      kDesktopBGColor);
+  FillRectangle(*pixel_writer,
+      { 0, kFrameHeight - 15 },
+      { kFrameWidth, 50 },
+      { 1, 8, 17 });
+  FillRectangle(*pixel_writer,
+      { 0, kFrameHeight - 15 },
+      { kFrameWidth / 5, 50 },
+      { 80, 80, 80 });
+  DrawRectangle(*pixel_writer,
+      { 10, kFrameHeight - 40 },
+      { 30, 30 },
+      { 160, 160, 160 });
+
+  console = new (console_buf) Console { *pixel_writer,
+    kDesktopFGColor, kDesktopBGColor };
+  printk("Welcome to MikanOS!\n");
+  SetLogLevel(kWarn);
+
+  mouser_cursor = new (mouse_cursor_buf) MouseCursor {
+    pixel_writer, kDesktopBGColor, { 300, 200 }
+  };
+
+  auto err = pci::ScanAllBus();
+  Log(kDebug, "ScanAllBus: %s\n", err.Name());
+
+  for (int i = 0; i < pci::num_device; ++i) {
+    const auto& dev = pci::devices[i];
+    auto vendor_id = pci::ReadVendorId(dev);
+    auto class_code = pci::ReadClassCode(dev.bus, dev.device, dev.function);
+    Log(kDebug, "%d.%d.%d: vend %04x, class %08x, head %02x\n",
+        dev.bus, dev.device, dev.function,
+        vendor_id, class_code, dev.header_type);
+  }
+
+  pci::Device* xhc_dev = nullptr;
+  for (int i = 0; i < pci::num_device; ++i) {
+    if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x30u)) {
+      xhc_dev = &pci::devices[i];
+
+      if (0x8086 == pci::ReadVendorId(*xhc_dev)) {
+        break;
+      }
     }
+  }
 
-    auto err = pci::ScanAllBus();
-    printk("ScanAllBus: %s\n", err.Name());
+  if (xhc_dev) {
+    Log(kInfo, "xHc has been found: %d.%d.%d\n", xhc_dev->bus, xhc_dev->device, xhc_dev->function);
+  }
 
-    for(int i = 0; i < pci::num_device; ++i) {
-        const auto& dev = pci::devices[i];
-        auto vendor_id = pci::ReadVendorId(dev.bus, dev.device, dev.function);
-        auto class_code = pci::ReadClassCode(dev.bus, dev.device, dev.function);
-        printk("%d.%d.%d: vend %04x, class %08x, head %02x\n",
-            dev.bus, dev.device, dev.function,
-            vendor_id, class_code, dev.header_type);
+  const WithError<uint64_t> xhc_bar = pci::ReadBar(*xhc_dev, 0);
+  Log(kDebug, "ReadBar: %s\n", xhc_bar.error.Name());
+  const uint64_t xch_mmio_base = xhc_bar.value & ~static_cast<uint64_t>(0xf);
+  Log(kDebug, "xHC mmio_base = %08lx\n", xch_mmio_base);
+
+  usb::xhci::Controller xhc { xch_mmio_base };
+
+  if (0x8086 == pci::ReadVendorId(*xhc_dev)) {
+    SwitchThci2Xhci(*xhc_dev);
+  }
+  {
+    auto err = xhc.Initialize();
+    Log(kDebug, "xhc.Initialize: %s\n", err.Name());
+  }
+
+  Log(kInfo, "xHC starting\n");
+  xhc.Run();
+
+  usb::HIDMouseDriver::default_observer = MouseObserver;
+
+  for (int i = 1; i <= xhc.MaxPorts(); ++i) {
+    auto port = xhc.PortAt(i);
+    Log(kDebug, "port %d: IsConnected=%d\n", i, port.IsConnected());
+
+    if (port.IsConnected()) {
+      if (auto err = ConfigurePort(xhc, port)) {
+        Log(kError, "failed to configure port: %s at %s:%d\n",
+            err.Name(), err.File(), err.Line());
+        continue;
+      }
     }
+  }
 
-    pci::Device* xhc_dev = nullptr;
-    for(int i = 0; i < pci::num_device; ++i) {
-        // if(pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x30u)) {
-        //     xhc_dev = &pci::device[i];
-
-        //     if(0x8086 == pci::ReadVendorId(*xhc_dev)){
-        //         break;
-        //     }
-        // }
+  while (1) {
+    if (auto err = ProcessEvent(xhc)) {
+      Log(kError, "Error while ProcessEvent: %d at %s:%d\n",
+          err.Name(), err.File(), err.Line());
     }
+  }
 
-    if(xhc_dev) {
-        // Log(kInfo, "xHc has been found: %d.%d.%d\n", xhc_dev->bus, xhc_dev->device, xhc_dev->function);
-    }
-    
-    while (1)
-        __asm__("hlt");
+  while (1)
+    __asm__("hlt");
+}
+
+extern "C" void _cxa_pure_virtual() {
+  while (1)
+    __asm__("hlt");
 }
