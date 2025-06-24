4ad9bc4e5f665b2ebac13156255237aa8cbdb557
arimura <33759663+arimura@users.noreply.github.com>
2025-06-24 09:38:13 +0900

    Update init tareget

--- docker.mk
+++ docker.mk
@@ -23,12 +23,19 @@ endif
 	/home/vscode/osbook/devenv/run_qemu.sh $(EFI) $(KERNEL_DIR)/$(KERNEL)
 
 # P51の設定も行う
-init: update-tools_def
+init: update-tools_def update-taget
 	ln -s $(MY_PKG) $(PKG_LINK)
 
 update-tools_def:
 	sed -i -e '/DEFINE CLANG38_X64_TARGET/s/-target x86_64-pc-linux-gnu/-target x86_64-linux-gnu/' \
        -e '/DEBUG_CLANG38_X64_CC_FLAGS/s/\([^[:cntrl:]]*\)/\1 -I\/usr\/x86_64-linux-gnu\/include/' /home/vscode/edk2/Conf/tools_def.txt
+
+update-taget:
+	sed -i \
+	  -e '/^ACTIVE_PLATFORM[[:space:]]*=/s|EmulatorPkg/EmulatorPkg.dsc|MikanLoaderPkg/MikanLoaderPkg.dsc|' \
+	  -e '/^TARGET_ARCH[[:space:]]*=/s|IA32|X64|' \
+	  -e '/^TOOL_CHAIN_TAG[[:space:]]*=/s|VS2015x86|CLANG38|' \
+	  /home/vscode/edk2/Conf/target.txt
 
 clean:
 	rm -f $(KERNEL_DIR)/$(KERNEL)
