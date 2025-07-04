5520a0ec4f1f0eea99059b2ca8e3999b5096918e
arimura <33759663+arimura@users.noreply.github.com>
2025-07-04 23:28:14 +0900

    Impl

--- my_mikanos/kernel/task.cpp
+++ my_mikanos/kernel/task.cpp
@@ -10,6 +10,11 @@ namespace {
     auto it = std::remove(c.begin(), c.end(), value);
     c.erase(it, c.end());
   }
+
+  void TaskIdle(uint64_t task_id, int64_t data) {
+    while (true)
+      __asm__("hlt");
+  }
 }
 
 Task::Task(uint64_t id)
@@ -77,6 +82,12 @@ TaskManager::TaskManager() {
                    .SetLevel(current_level_)
                    .SetRunning(true);
   running_[current_level_].push_back(&task);
+
+  Task& idle = NewTask()
+                   .InitContext(TaskIdle, 0)
+                   .SetLevel(0)
+                   .SetRunning(true);
+  running_[0].push_back(&idle);
 }
 
 Task& TaskManager::NewTask() {
