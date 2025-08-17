50b8d2edd0e3470bb05c365ad9c0518d66138f65
arimura <33759663+arimura@users.noreply.github.com>
2025-08-17 10:56:43 +0900

    impl

--- my_mikanos/kernel/message.hpp
+++ my_mikanos/kernel/message.hpp
@@ -5,7 +5,8 @@
 enum class LayerOperation {
   Move,
   MoveRelative,
-  Draw
+  Draw,
+  DrawArea
 };
 
 struct Message {
@@ -35,6 +36,7 @@ struct Message {
       LayerOperation op;
       unsigned int layer_id;
       int x, y;
+      int w, h;
     } layer;
   } arg;
 };
